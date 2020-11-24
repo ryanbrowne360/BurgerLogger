094b0b8efc617e66a35d6b3dc9030cc93da01c34
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-23 20:27:43 -0500

    added files from cat activity to model from

---
+++ config/burgers_db.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+CREATE TABLE burger_type(
+    id int,
+    name varchar(255),
+    devour boolean
+);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require( "dotenv" ).config()
 const express = require("express")
 
 const PORT = process.env.PORT || 8080
-const app = express
+const app = express()
 
 app.use(express.static("public"));
 
@@ -14,9 +14,9 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgers_controller.js");
+// var routes = require("./controllers/burgers_controller.js");
 
-app.use(routes);
+// app.use(routes);
 
 app.listen(PORT, function() {
  
