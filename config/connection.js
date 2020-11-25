24bdaa773df931152c62e97fba71c939baea292f
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-25 11:18:02 -0500

    deployed heroku working!

--- config/connection.js
+++ config/connection.js
@@ -28,7 +28,7 @@ class Database {
   }
 }
 
-const ORMconnection = new Database(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : {
+const ORMconnection = new Database({
   host: "r1bsyfx4gbowdsis.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   user: "awnd1y1gogl85lf7",
