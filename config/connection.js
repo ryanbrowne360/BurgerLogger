38dfbf36415f327bb5ac2cd48dcc49342695eeb3
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-25 10:35:43 -0500

    changes to connection

--- config/connection.js
+++ config/connection.js
@@ -1,70 +1,41 @@
 // Set up MySQL ORMconnectionion.
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-  ORMconnection = mysql.createORMconnectionion(process.env.JAWSDB_URL)
-} else {
-  ORMconnection = mysql.createORMconnectionion({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "1234",
-    database: "burgers_db"
-  });
+
+class Database {
+  constructor(config) {
+    this.connection = mysql.createConnection(config);
+  }
+  query(sql, args = []) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(sql, args, (err, rows) => {
+        if (err) 
+          return reject(err);
+
+        resolve(rows);
+      });
+    });
+  }
+  close() {
+    return new Promise((resolve, reject) => {
+      this.connection.end(err => {
+        if (err) 
+          return reject(err);
+
+        resolve();
+      });
+    });
+  }
 }
 
-ORMconnection.connect();
+const ORMconnection = new Database(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : {
+  host: "localhost",
+  port: 3306,
+  user: "awnd1y1gogl85lf7",
+  password: "mdhigivzpj0y9dss",
+  database: "burgers_db"
+});
+
+
 
 module.exports = ORMconnection;
-
-// ORMconnection.connect(function (err) {
-  //   if (err) {
-  //     console.error("errir ORMconnectioning: " + err.stack);
-  //     return;
-  //   }
-  //   console.log("ORMconnectioned as id" + ORMconnection.threadId);
-  // });
-  
-
-// var ORMconnection = new Database(process.env.JAWSDB_URL ? process.env.JAWSDB_URL : {
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "1234",
-//   database: "burgers_db"
-// });
-
-
-// class Database {
-//   constructor(config) {
-//     this.ORMconnectionion = mysql.createORMconnectionion(config);
-//   }
-//   query(sql, args = []) {
-//     return new Promise((resolve, reject) => {
-//       this.ORMconnectionion.query(sql, args, (err, rows) => {
-//         if (err) 
-//           return reject(err);
-
-//         resolve(rows);
-//       });
-//     });
-//   }
-//   close() {
-//     return new Promise((resolve, reject) => {
-//       this.ORMconnectionion.end(err => {
-//         if (err) 
-//           return reject(err);
-
-//         resolve();
-//       });
-//     });
-//   }
-// }
-
-
-
-
-
-
-
-
